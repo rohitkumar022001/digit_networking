a6b07839bb6c29554637c46712dca47dbaf92e82
Rohit.Kumar02 <Rohit.Kumar02@LDIGITBGL-L0215.godigit.corp>
2023-07-27 11:11:56 +0530

    first commit

---
+++ src/main/java/com/digit/digit_networking/Client1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.digit.digit_networking;
+
+/**
+ * 
+ */
+public class Client1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
