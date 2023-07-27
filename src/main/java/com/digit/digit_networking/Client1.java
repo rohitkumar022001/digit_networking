2854e17b20b83e045d8e7b540256b3bc93d7c99b
Rohit.Kumar02 <Rohit.Kumar02@LDIGITBGL-L0215.godigit.corp>
2023-07-27 11:24:56 +0530

    server client commit

--- src/main/java/com/digit/digit_networking/Client1.java
+++ src/main/java/com/digit/digit_networking/Client1.java
@@ -3,6 +3,11 @@
  */
 package com.digit.digit_networking;
 
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -11,8 +16,16 @@ public class Client1 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		Socket csoc=new Socket("10.1.131.203",4000);
+		OutputStream os=csoc.getOutputStream();
+		DataOutputStream dos=new DataOutputStream(os);
+		Scanner sc=new Scanner(System.in);
+		String msg=sc.nextLine();
+		dos.writeUTF(msg);
+		
+		
 
 	}
 
